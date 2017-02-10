97c9819e51abdbf33a3e03acbd24facc9893fdb3
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-10 14:37:21 +0800

    Change isbn data type from integer to string.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170210062713) do
+ActiveRecord::Schema.define(version: 20170210063604) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
