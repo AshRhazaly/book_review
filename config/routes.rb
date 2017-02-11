c5f981d61164a97b2335de52c6a4a4641535a275
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-11 23:17:21 +0800

    Nest reviews within books and add search.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :authors
-  resources :books
+  resources :books do
+      resources :reviews
+      collection do
+        get 'search'
+        post 'search'
+      end
+  end
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
