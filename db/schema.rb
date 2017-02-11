6bf4e4f0157a5edebb27883504bb518d5ae2552a
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-11 22:20:42 +0800

    Migrate database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211095429) do
+ActiveRecord::Schema.define(version: 20170211141927) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170211095429) do
     t.string   "isbn"
     t.string   "category"
     t.integer  "book_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
