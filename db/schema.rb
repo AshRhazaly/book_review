660bb7337fa893a2248e6cb63a6862ae63f74895
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-11 22:16:34 +0800

    Remove author model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211095737) do
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.text     "books_written"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
+ActiveRecord::Schema.define(version: 20170211095429) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
