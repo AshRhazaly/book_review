b2f5f0814a28ea654d9000b7be0396225c5296ab
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-11 17:58:06 +0800

    Initialize Author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211095429) do
+ActiveRecord::Schema.define(version: 20170211095737) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.text     "books_written"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
